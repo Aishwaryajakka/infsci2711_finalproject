f6837949734c1424ab01040face3ffd015d44ac5
Aishwarya Jakka <aishwaryajakka@hotmail.com>
2020-04-10 12:15:25 -0400

    added files

---
+++ sql/queries.sql
@@ -0,0 +1,11 @@
+/* Query 1: What time of the day is the sale maximum per country? */
+/* Query 2: Annual total sales of per product */
+/* Query 3: Top product per year */
+/* Query 4: Top product per country */
+/*Query 5: Which item is sold below the threshold value (under-performed products) */
+/*Query 6: Which customer spends the most (per country or overall) */
+/*Query 7: Best-selling month (per country) [2009-2011] */
+/*Query 8: Best-selling product per month [2009-2011] */
+/*Query 9: What is the change in total sales per country per year (trend of sales) */
+/*Query 10: Average spending of a customer in that country (Total sales/ no of customer) */
+/*Query 11: Frequently purchased item per customer */
---
+++ sql/view_materialisation.sql
@@ -0,0 +1,1 @@
+/* Materialised Views go here */
