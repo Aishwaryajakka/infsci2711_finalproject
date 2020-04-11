299ba1700bd31ea04d671b72743008137ffdb07f
Aishwarya Jakka <aishwaryajakka@hotmail.com>
2020-04-11 06:23:49 -0400

    Checked and added corrections to the queries

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,61 +1,116 @@
+/*
+ THIS FILE CONTAINS THE LIST OF QUERIES USING MATERIALISED VIEWS
+*/
+
+/* ****************************************************************************************************************** */
 /* Query 1: What time of the day is the sale maximum per country? */
-/*SQL Query */
 
-select Hour, max(Total) as max_total_sale, Country from(
-SELECT  fact.Hour as Hour ,sum(fact.TotalSale) as total, customerdim.Country as Country 
-from fact  JOIN customerdim ON  fact.Customer_ID=customerdim.Customer_ID  GROUP BY
- fact.Hour,customerdim.Country) as a group by country
+/*SQL Query (working) */
 
-/* Query using materialized view */
-select Hour,max(TotalSale) as max_total_sale, Country from mview_sum_sale_country group by country
+select a.Hour, a.Total as max_total_sale, a.Country from
+(
+ SELECT  fact.Hour as Hour ,sum(fact.TotalSale) as total, customerdim.Country as Country 
+from fact JOIN customerdim ON  fact.Customer_ID=customerdim.Customer_ID
+GROUP BY fact.Hour,customerdim.Country) as a
+INNER JOIN
+ ( select  max(total) as maxtotal, country from (
+ SELECT  fact.Hour as Hour ,sum(fact.TotalSale) as total, customerdim.Country as Country 
+from fact JOIN customerdim ON  fact.Customer_ID=customerdim.Customer_ID
+GROUP BY fact.Hour,customerdim.Country ) t1 
+group by country ) as b 
+ON a.total= b.maxtotal and a.country = b.country;
+
+/* Query using materialized view (working) */
+
+select f.hour, f.totalsale, f.Country
+from (
+   select  max(totalsale)as total,country  from mview_sum_sale_country group by country
+
+) as x inner join mview_sum_sale_country as f on f.totalsale = x.total and x.country = f.country;
+/* ****************************************************************************************************************** */
 
 /* Query 2: Annual total sales of per product */
 
-/*SQL Query */ 
-select fact.StockCode, fact.year, sum(fact.TotalSale), stockdim.Description from fact join stockdim on fact.StockCode = stockdim.StockCode group by StockCode, year
+/*SQL Query  (working)*/ 
 
-/* Query using materialized view */
-Select * from m_view_totalsale_product
+select fact.StockCode, fact.year, sum(fact.TotalSale), stockdim.Description 
+from fact join stockdim on 
+fact.StockCode = stockdim.StockCode 
+group by stockdim.StockCode, fact.year, stockdim.Description;
+
+/* Query using materialized view (working) */
+Select * from mview_totalsale_product;
+
+/* ****************************************************************************************************************** */
 
 /* Query 3: Top product per year */
 
+/*SQL Query (working) */
+select f.stockCode, f.description,f.Year, f.TotalSale as totalSale
+from (
+  SELECT Year, max(Totalsale) as maxtotal FROM (select stockdim.StockCode, fact.year as year,  stockdim.Description ,sum(fact.TotalSale) as TotalSale from fact join stockdim on fact.StockCode = stockdim.StockCode group by Stockdim.StockCode, Fact.Year, stockdim.Description
+)t1 group by Year)
+ as x inner join ( select stockdim.StockCode, fact.year,  stockdim.Description ,sum(fact.TotalSale)from fact join stockdim on fact.StockCode = stockdim.StockCode group by Stockdim.StockCode, Fact.Year, stockdim.Description
+)as f on f.totalsale = x.maxtotal and x.year = f.year;
+
+/* Query using materialized view: (working) */
+
+select f.stockCode, f.description,f.Year, f.TotalSale as totalSale
+from (
+  SELECT Year, max(Totalsale) as maxtotal FROM mview_totalsale_product group by Year
+
+) as x inner join mview_totalsale_product as f on f.totalsale = x.maxtotal and x.year = f.year;
+
+/* ****************************************************************************************************************** */
+
+/* Query 4: Top product  per country */
+
+/*Query using materialized view: (working) */
+select f.stockCode, f.Country, f.Quantity as Total
+from (
+  SELECT Country, max(Quantity) as maxtotal FROM mview_product_country group by Country
+
+) as x inner join mview_product_country as f on f.Quantity = x.maxtotal and x.Country = f.Country
+order by total desc
+
+
+/* ****************************************************************************************************************** */
+
+
+/*Query 5: Which item is sold below the threshold value (under-performed products) in 2010 */
+
+
 /*SQL Query */
-select years, max(total), description from (select fact.StockCode, fact.year, sum(fact.TotalSale), stockdim.Description from fact join stockdim on fact.StockCode = stockdim.StockCode group by StockCode, year ) as total group by years
-
-/* Query using materialized view: */
-
-SELECT stockCode, Year, description ,max(Totalsale) FROM online_retail_adb.mview_totalsale_product group by Year
 
 
-/* Query 4: Top product per country */
-/*SQL Query */
-use retail_database;
-      use retail_database;
-      select Country, max(total) from (select
-         fact.StockCode as StockCode,
+/*Query using materialized view: */
+
+
+
+/* ****************************************************************************************************************** */
+
+
+/*Query 6: Which customer spends the most (per country or overall) */
+/*SQL Query  (working) but very expensive */
+select f.customer_id, f.totalsale, f.Country
+from (
+   select  max(totalsale) as maxtotal ,country  from (
+   select
+         fact.Customer_ID as customerID,
          customerdim.Country as country,
-         sum(fact.Quantity) as total
+         sum(fact.TotalSale) as totalSale
       from
          fact 
          join
             customerdim
             on fact.Customer_ID = customerdim.Customer_ID
       group by
-         StockCode,
-         country) as total group by Country
+         fact.Customer_ID,
+         customerdim.Country) t1
+   group by country
 
-
-/*Query using materialized view: */
-
-SELECT StockCode, Country, max(Quantity) as maxSale FROM online_retail_adb.mview_product_country group by Country
-/*Query 5: Which item is sold below the threshold value (under-performed products) */
-/*SQL Query */
-
-/*Query using materialized view: */
-/*Query 6: Which customer spends the most (per country or overall) */
-/*SQL Query */
-SELECT CustomerID, country, max(totalSale) FROM 
-(select
+) as x inner join
+ (select
          fact.Customer_ID as customerID,
          customerdim.Country as country,
          sum(fact.TotalSale) as totalSale
@@ -67,57 +122,83 @@ SELECT CustomerID, country, max(totalSale) FROM
       group by
          fact.Customer_ID,
          customerdim.Country
-)as a group by country
+ ) as f on f.totalsale = x.maxtotal and x.country = f.country;
 
 
-/*Query using materialized view: */
-SELECT Customer_ID, Country, max(TotalSale) FROM online_retail_adb.mview_customer_total_country group by Country
+/*Query using materialized view: (working) */
+/* Top Spending customer id per country */
+select f.customer_id, f.totalsale, f.Country
+from (
+   select  max(totalsale)as maxtotal ,country  from mview_customer_total_country group by country
 
+) as x inner join mview_customer_total_country as f on f.totalsale = x.maxtotal and x.country = f.country;
+
+/* ****************************************************************************************************************** */
 
 /*Query 7: Best-selling month (per country) [2009-2011] */
-/*SQL Query */
-select month, country,max(totalsale) from (
-select
-         fact.Month as Month,
-         customerdim.Country as country,
-         sum(fact.TotalSale) as totalSale
-      from
-         fact 
-         join
-            customerdim
-            on fact.Customer_ID = customerdim.Customer_ID
-      group by
-         Month,
-         country
-     )    as a
-group by country
 
+/*Query using materialized view: (working)*/
 
-/*Query using materialized view: */
+select f.month, f.totalsale, f.Country
+from (
+   select  max(totalsale) as maxtotal ,country  from mview_sale_month group by country
 
-SELECT month, country,max(totalsale) FROM online_retail_adb.mview_sale_month group by country
+) as x inner join mview_sale_month as f on f.totalsale = x.maxtotal and x.country = f.country;
+
+/* ****************************************************************************************************************** */
+
 
 /*Query 8: Best-selling product per month [2009-2011] */
-/*SQL Query */
-select StockCode, Month, max(totalQuantity) as Quantity from (
-SELECT fact.StockCode as StockCode, fact.Month as Month, sum(fact.Quantity) as totalQuantity FROM fact group by fact.Month, fact.StockCode) as a group by StockCode,Month;
-
 
 /*Query using materialized view: */
+select t2.StockCode, t2.description, t2.Month, t2.TotalQuantity
+from
+(select StockCode, description, max(totalquantity) as maxtotal  from mview_quantity_product 
+group by StockCode, description) as t1 inner join mview_quantity_product t2 
+on t2.stockCode=t1.stockcode and t2.description=t1.description and t2.TotalQuantity = t1.maxtotal;
+
+/* ****************************************************************************************************************** */
+
 /*Query 9: What is the change in total sales per country per year (trend of sales) */
 /*SQL Query */
 
 /*Query using materialized view: */
+/* ****************************************************************************************************************** */
+
 /*Query 10: Average spending of a customer in that country (Total sales/ no of customer) */
-/*SQL Query */
-select (sum(TotalSale)/count(Customer_ID)) as Avg_Spending, country from (select fact.Customer_ID, sum(fact.TotalSale) as TotalSale, customerdim.Country from fact join customerdim on fact.Customer_ID = customerdim.Customer_ID group by fact.Customer_ID, customerdim.Country) as a group by Country
+/*SQL Query (working)*/
+select (sum(TotalSale)/count(Customer_ID)) as Avg_Spending, country 
+from (
+   select fact.Customer_ID, sum(fact.TotalSale) as TotalSale, customerdim.Country 
+   from fact join customerdim on fact.Customer_ID = customerdim.Customer_ID
+   group by fact.Customer_ID, customerdim.Country) as a 
+group by Country;
 
-/*Query using materialized view: */
-select (count(Customer_ID)/ sum(TotalSale)) as Avg_Spending, country from mview_customer_total_country group by Country
-                                
+/*Query using materialized view: (working)*/
+select (count(Customer_ID)/ sum(TotalSale)) as Avg_Spending, country 
+from mview_customer_total_country 
+group by Country;
+
+
+/* ****************************************************************************************************************** */
+
 /*Query 11: Frequently purchased item per customer */
-/*SQL Query */
-select fact.Customer_ID, fact.StockCode, count(fact.StockCode) from fact group by fact.Customer_ID
+/*SQL Query (working) */
+select f.Customer_ID, f.stockCode,f.stockcount
+from (
+select max(stockcount) as maxstockcount , Customer_ID
+from (select Customer_ID, stockCode, count(stockcode) as stockcount
+from fact 
+group by Customer_ID, stockCode) t1
+group by Customer_ID ) as x 
+INNER JOIN ( select Customer_ID, stockCode, count(stockcode) as stockcount
+from fact 
+group by Customer_ID, stockCode) as f 
+on f.stockcount = x.maxstockcount and f.customer_id = x.customer_id;
+
+
 
 /*Query using materialized view: */
-No need for using materialized view
+/* No need for using materialized view */
+
+/* ****************************************************************************************************************** */
--- sql/view_materialisation.sql
+++ sql/view_materialisation.sql
@@ -1,11 +1,15 @@
-/* Materialised Views go here */
+/* 
+   MATERIALISED VIEWS 
+   ** please note: Execute this script in online_retail_adb database first before executing the queries 
+*/
 /* Create materialized view mview_sum_sale_country */
 
 create table mview_sum_sale_country (Hour int(2), TotalSale decimal(32,2), Country varchar(100));
 
 /*insert into materialized view */
 
-insert into mview_sum_sale_country (SELECT  fact.Hour ,sum(fact.TotalSale) as total, customerdim.Country 
+insert into mview_sum_sale_country (
+   select fact.Hour ,sum(fact.TotalSale) as total, customerdim.Country 
 from fact
  JOIN customerdim
 ON
@@ -19,41 +23,57 @@ create table mview_totalsale_product(
 stockCode varchar(15),
 Year int(4),
 description varchar(100),
-Totalsale numeric(10,2))
+Totalsale numeric(10,2));
 
 
-/*  insert into view mview_totalsale_produc */
+/*  insert into view mview_totalsale_product */
 insert into mview_totalsale_product(
-select fact.StockCode, fact.year,  stockdim.Description ,sum(fact.TotalSale)from fact join stockdim on fact.StockCode = stockdim.StockCode group by StockCode, year
-)
+select stockdim.StockCode, fact.year,  stockdim.Description ,sum(fact.TotalSale)from fact join stockdim on fact.StockCode = stockdim.StockCode group by Stockdim.StockCode, Fact.Year, stockdim.Description
+);
 
+/*  Create materialized view mview_quantity_product */
+
+create table mview_quantity_product(
+stockCode varchar(15),
+Month int(2),
+description varchar(100),
+TotalQuantity int(11));
+
+
+/*  insert into view mview_quantity_product */
+insert into mview_quantity_product(
+Select stockdim.StockCode, fact.month,  stockdim.Description ,sum(fact.Quantity) 
+    from fact join stockdim on fact.StockCode = stockdim.StockCode
+    group by Stockdim.StockCode, Fact.month, stockdim.Description
+);
 
 /* Create materialized view mview_product_country: */
+/* Ideally we would like to display description but due to the complexity and heaviness of the query,
+ we shall display only stockcode */
 
 create table mview_product_country
 (
 StockCode varchar(15),
 Country varchar(100),
 Quantity int(11)
-)
+);
 
 
 /* Insert Materialized view mview_product_country:  */
-
-insert into mview_product_country(
+insert into mview_product_country (
 select
-         fact.StockCode as StockCode,
-         customerdim.Country as country,
-         sum(fact.Quantity) as total
+         f.StockCode as StockCode,
+         c.Country as country,
+         sum(f.Quantity) as total
       from
-         fact 
-         join
-            customerdim
-            on fact.Customer_ID = customerdim.Customer_ID
+         FACT f
+         inner join
+            customerdim c
+            on f.Customer_ID = c.Customer_ID
       group by
-         StockCode,
-         country 
-)
+         f.StockCode,
+        c.country
+);
 
 
 
@@ -63,7 +83,7 @@ create table mview_customer_total_country(
 Customer_ID varchar(15),
 Country varchar(100),
 TotalSale numeric(10,2)
-)
+);
 
 /* Insert into view mview_customer_total_country */
 
@@ -76,11 +96,10 @@ insert into mview_customer_total_country
          fact 
          join
             customerdim
-            on fact.Customer_ID = customerdim.Customer_ID)
+            on fact.Customer_ID = customerdim.Customer_ID
       group by
          fact.Customer_ID,
-         customerdim.Country
-
+         customerdim.Country);
 
 
 /*  create Materialized view mview_sale_month: */
@@ -89,7 +108,7 @@ create table mview_sale_month(
 month int(2),
 country varchar(100),
 totalsale numeric(10,2)
-)
+);
 
 /* Insert into materialized view mview_sale_month:  */
 
@@ -105,9 +124,9 @@ select
             customerdim
             on fact.Customer_ID = customerdim.Customer_ID
       group by
-         Month,
-         country
-     )    
+         fact.Month,
+         CUSTOMERDIM.Country
+     );   
      
 
 
@@ -116,10 +135,13 @@ select
 create table mview_yearsale_country(
 Year int(4),
 totalSale numeric(10,2),
-Country varchar(100))
+Country varchar(100));
 
 
 /* Insert into materialized view mview_yearsale_country:  */
 insert into mview_yearsale_country(
-SELECT fact.Year, sum(fact.TotalSale), customerdim.Country from online_retail_adb.fact join online_retail_adb.customerdim on fact.Customer_ID=customerdim.Customer_ID group by fact.year, customerdim.Country
-)
+SELECT fact.Year, sum(fact.TotalSale), customerdim.Country 
+from online_retail_adb.fact join online_retail_adb.customerdim
+on fact.Customer_ID=customerdim.Customer_ID 
+group by fact.year, customerdim.Country
+);
