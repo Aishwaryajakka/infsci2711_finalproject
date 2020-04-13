ee72ea2ed95b01eeb8fbf1050e6c0c6b5c27d629
blackreaper2396 <sohambhatnagar23@gmail.com>
2020-04-13 13:54:07 -0400

    Revert "Revert "Merge branch 'master' into neo4j_db""
    This reverts commit 4e631202189dbda718b1d66b93a7c15d752edca3.

---
+++ frontend/README.md
@@ -0,0 +1,3 @@
+Run "node app.js" in terminal while pointing to this folder
+
+Go to http://localhost:3000/ on any browser
---
+++ frontend/routes/sql.js
@@ -0,0 +1,119 @@
+/**
+ * Created on April 12, 2020
+ */
+const express = require("express");
+const router = express.Router();
+require("../public/sqlQueries.js");
+
+
+router.get("/query1", function (req, res) {
+    const con = req.app.locals.con;
+    con.query(sqlQueries["query1"], function (err, rows) {
+        //console.log(rows);
+        res.render("sql", {
+            data: rows
+        });
+    });
+});
+
+router.get("/query2", function (req, res) {
+    const con = req.app.locals.con;
+    con.query(sqlQueries["query2"], function (err, rows) {
+        //console.log(rows);
+        res.render("sql", {
+            data: rows
+        });
+    });
+});
+
+router.get("/query3", function (req, res) {
+    const con = req.app.locals.con;
+    con.query(sqlQueries["query3"], function (err, rows) {
+        //console.log(rows);
+        res.render("sql", {
+            data: rows
+        });
+    });
+});
+
+router.get("/query4", function (req, res) {
+    const con = req.app.locals.con;
+    con.query(sqlQueries["query4"], function (err, rows) {
+        //console.log(rows);
+        res.render("sql", {
+            data: rows
+        });
+    });
+});
+
+router.get("/query5", function (req, res) {
+    const con = req.app.locals.con;
+    con.query(sqlQueries["query5"], function (err, rows) {
+        //console.log(rows);
+        res.render("sql", {
+            data: rows
+        });
+    });
+});
+
+router.get("/query6", function (req, res) {
+    const con = req.app.locals.con;
+    con.query(sqlQueries["query6"], function (err, rows) {
+        //console.log(rows);
+        res.render("sql", {
+            data: rows
+        });
+    });
+});
+
+router.get("/query7", function (req, res) {
+    const con = req.app.locals.con;
+    con.query(sqlQueries["query7"], function (err, rows) {
+        //console.log(rows);
+        res.render("sql", {
+            data: rows
+        });
+    });
+});
+
+router.get("/query8", function (req, res) {
+    const con = req.app.locals.con;
+    con.query(sqlQueries["query8"], function (err, rows) {
+        //console.log(rows);
+        res.render("sql", {
+            data: rows
+        });
+    });
+});
+
+router.get("/query9", function (req, res) {
+    const con = req.app.locals.con;
+    con.query(sqlQueries["query9"], function (err, rows) {
+        //console.log(rows);
+        res.render("sql", {
+            data: rows
+        });
+    });
+});
+
+router.get("/query10", function (req, res) {
+    const con = req.app.locals.con;
+    con.query(sqlQueries["query10"], function (err, rows) {
+        //console.log(rows);
+        res.render("sql", {
+            data: rows
+        });
+    });
+});
+
+router.get("/query11", function (req, res) {
+    const con = req.app.locals.con;
+    con.query(sqlQueries["query11"], function (err, rows) {
+        //console.log(rows);
+        res.render("sql", {
+            data: rows
+        });
+    });
+});
+
+module.exports = router;
